83e2fd934cc8599e864f0ba049cf944df51a8b6b
lxx9525 <lxx9525@closeli.cn>
2016-12-21 23:00:49 +0800

    modify

--- CocoaCategory-OC/CocoaCategory-OC/Category/NSDate+LucioCategory.h
+++ CocoaCategory-OC/CocoaCategory-OC/Category/NSDate+LucioCategory.h
@@ -11,43 +11,43 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface NSDate (LucioCategory)
 
-@property (nonatomic, readonly) NSInteger year;
-@property (nonatomic, readonly) NSInteger month;
-@property (nonatomic, readonly) NSInteger day;
-@property (nonatomic, readonly) NSInteger hour;
-@property (nonatomic, readonly) NSInteger minute;
-@property (nonatomic, readonly) NSInteger second;
-@property (nonatomic, readonly) NSInteger nanosecond;
-@property (nonatomic, readonly) NSInteger weekday;
-@property (nonatomic, readonly) NSInteger weekdayOrdinal;
-@property (nonatomic, readonly) NSInteger weekOfMonth;
-@property (nonatomic, readonly) NSInteger weekOfYear;
-@property (nonatomic, readonly) NSInteger yearForWeekOfYear;
-@property (nonatomic, readonly) NSInteger quarter;
-@property (nonatomic, readonly) BOOL isLeapMonth;
-@property (nonatomic, readonly) BOOL isLeapYear;
-@property (nonatomic, readonly) BOOL isToday;
-@property (nonatomic, readonly) BOOL isYesterday;
+@property (nonatomic, readonly) NSInteger lc_year;
+@property (nonatomic, readonly) NSInteger lc_month;
+@property (nonatomic, readonly) NSInteger lc_day;
+@property (nonatomic, readonly) NSInteger lc_hour;
+@property (nonatomic, readonly) NSInteger lc_minute;
+@property (nonatomic, readonly) NSInteger lc_second;
+@property (nonatomic, readonly) NSInteger lc_nanosecond;
+@property (nonatomic, readonly) NSInteger lc_weekday;
+@property (nonatomic, readonly) NSInteger lc_weekdayOrdinal;
+@property (nonatomic, readonly) NSInteger lc_weekOfMonth;
+@property (nonatomic, readonly) NSInteger lc_weekOfYear;
+@property (nonatomic, readonly) NSInteger lc_yearForWeekOfYear;
+@property (nonatomic, readonly) NSInteger lc_quarter;
+@property (nonatomic, readonly) BOOL lc_isLeapMonth;
+@property (nonatomic, readonly) BOOL lc_isLeapYear;
+@property (nonatomic, readonly) BOOL lc_isToday;
+@property (nonatomic, readonly) BOOL lc_isYesterday;
 
-- (nullable NSDate *)dateByAddingYears:(NSInteger)years;
-- (nullable NSDate *)dateByAddingMonths:(NSInteger)months;
-- (nullable NSDate *)dateByAddingWeeks:(NSInteger)weeks;
-- (nullable NSDate *)dateByAddingDays:(NSInteger)days;
-- (nullable NSDate *)dateByAddingHours:(NSInteger)hours;
-- (nullable NSDate *)dateByAddingMinutes:(NSInteger)minutes;
-- (nullable NSDate *)dateByAddingSeconds:(NSInteger)seconds;
+- (nullable NSDate *)lc_dateByAddingYears:(NSInteger)years;
+- (nullable NSDate *)lc_dateByAddingMonths:(NSInteger)months;
+- (nullable NSDate *)lc_dateByAddingWeeks:(NSInteger)weeks;
+- (nullable NSDate *)lc_dateByAddingDays:(NSInteger)days;
+- (nullable NSDate *)lc_dateByAddingHours:(NSInteger)hours;
+- (nullable NSDate *)lc_dateByAddingMinutes:(NSInteger)minutes;
+- (nullable NSDate *)lc_dateByAddingSeconds:(NSInteger)seconds;
 
-- (nullable NSString *)stringWithFormat:(NSString *)format;
-- (nullable NSString *)stringWithFormat:(NSString *)format
+- (nullable NSString *)lc_stringWithFormat:(NSString *)format;
+- (nullable NSString *)lc_stringWithFormat:(NSString *)format
                                timeZone:(nullable NSTimeZone *)timeZone
                                  locale:(nullable NSLocale *)locale;
-- (nullable NSString *)stringWithISOFormat;
-+ (nullable NSDate *)dateWithString:(NSString *)dateString format:(NSString *)format;
-+ (nullable NSDate *)dateWithString:(NSString *)dateString
+- (nullable NSString *)lc_stringWithISOFormat;
++ (nullable NSDate *)lc_dateWithString:(NSString *)dateString format:(NSString *)format;
++ (nullable NSDate *)lc_dateWithString:(NSString *)dateString
                              format:(NSString *)format
                            timeZone:(nullable NSTimeZone *)timeZone
                              locale:(nullable NSLocale *)locale;
-+ (nullable NSDate *)dateWithISOFormatString:(NSString *)dateString;
++ (nullable NSDate *)lc_dateWithISOFormatString:(NSString *)dateString;
 
 
 @end
