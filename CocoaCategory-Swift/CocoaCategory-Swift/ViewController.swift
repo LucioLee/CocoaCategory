0b03d52173c255d364a9eb091f80e28d0834e464
lxx9525 <lxx9525@closeli.cn>
2016-12-26 17:56:31 +0800

    modify code

--- CocoaCategory-Swift/CocoaCategory-Swift/ViewController.swift
+++ CocoaCategory-Swift/CocoaCategory-Swift/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let view = UIView()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
